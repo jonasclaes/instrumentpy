e266a9a5c7a9d35584f94f733914d798326dc34b
Jonas Claes <jonas@jonasclaes.be>
2022-08-06 21:10:46 +0200

    Added interface management commands to AIM/TTi MX100TP

--- src/instrumentpy/psu/aimtti/mx100tp.py
+++ src/instrumentpy/psu/aimtti/mx100tp.py
@@ -120,7 +120,7 @@ class MX100TP():
 
     # Status commands
 
-    def clear_status(self):
+    def clear_status(self) -> None:
         """
         Implemented command: *CLS
 
@@ -139,7 +139,7 @@ class MX100TP():
         self._device.send_command(f"LSR{register_number}?\n")
         return self._device.receive_output()
 
-    def set_limit_status_enable_register(self, register_number: int, limit: int | float) -> str:
+    def set_limit_status_enable_register(self, register_number: int, limit: int | float) -> None:
         """
         Implemented command: LSE<N> <NRF>
 
@@ -189,7 +189,7 @@ class MX100TP():
         self._device.send_command("*STB?\n")
         return self._device.receive_output()
 
-    def set_service_request_enable_register(self, value: int | float) -> str:
+    def set_service_request_enable_register(self, value: int | float) -> None:
         """
         Implemented command: *SRE <NRF>
 
@@ -207,7 +207,7 @@ class MX100TP():
         self._device.send_command("*SRE?\n")
         return self._device.receive_output()
 
-    def set_parallel_poll_enable_register(self, value: int | float) -> str:
+    def set_parallel_poll_enable_register(self, value: int | float) -> None:
         """
         Implemented command: *PRE <NRF>
 
@@ -236,3 +236,81 @@ class MX100TP():
         return self._device.receive_output()
 
     # Interface Management Commands
+
+    def go_to_local(self) -> None:
+        """
+        Implemented command: LOCAL
+
+        Go to local. Any subsequent command will restore the remote state.
+        """
+        self._device.send_command("LOCAL\n")
+
+    def set_interface_lock(self, state: int) -> None:
+        """
+        Implemented command: IFLOCK <NRF>
+
+        Set or Clear the lock requiring the instrument to respond only to this
+        interface, where <NRF> has the meaning: 0 = clear and 1 = set the lock.
+        It is an Execution Error (number 200) if the request is denied either because
+        of conflict with a lock on this or another interface, or the user has disabled
+        this interface from taking control using the web interface.
+        """
+        self._device.send_command(f"IFLOCK;{state}\n")
+
+    def get_interface_lock(self) -> str:
+        """
+        Implemented command: IFLOCK?
+
+        Query the status of the interface lock.
+        The response is: <NR1><RMT> where <NR1> is
+        = 0 if there is no active lock,
+        = 1 if this interface instance owns the lock or
+        = -1 if the lock is unavailable either because it is in use by another interface
+        or the user has disabled this interface from taking control (via the web
+        interface).
+        """
+        self._device.send_command("IFLOCK?\n")
+        return self._device.receive_output()
+
+    def get_interface_address(self) -> str:
+        """
+        Implemented command: ADDRESS?
+
+        Returns the GPIB bus Address. The response is <NR1><RMT>.
+        """
+        self._device.send_command("ADDRESS?\n")
+        return self._device.receive_output()
+
+    def get_ip_address(self) -> str:
+        """
+        Implemented command: IPADDR?
+
+        Returns the present IP address of the LAN interface, provided it is
+        connected.
+        If it is not connected, the response will be the static IP if configured to
+        always use that static IP, otherwise it will be 0.0.0.0 if waiting for DHCP or
+        Auto-IP.
+        The response is nnn.nnn.nnn.nnn<RMT>, where each nnn is 0 to 255.
+        """
+        self._device.send_command("IPADDR?\n")
+        return self._device.receive_output()
+
+    def get_netmask(self) -> str:
+        """
+        Implemented command: NETMASK?
+
+        Returns the present netmask of the LAN interface, provided it is connected.
+        The response is nnn.nnn.nnn.nnn<RMT>, where each nnn is 0 to 255.
+        """
+        self._device.send_command("NETMASK?\n")
+        return self._device.receive_output()
+
+    def get_netconfig(self) -> str:
+        """
+        Implemented command: NETCONFIG?
+
+        Returns the first means by which an IP address will be sought.
+        The response is <CRD><RMT > where <CRD> is DHCP, AUTO or STATIC.
+        """
+        self._device.send_command("NETCONFIG?\n")
+        return self._device.receive_output()
